13b8365d743f0a7b8980be39469057741b82a449
LeapSunrise <leap_sunrise1320@ukr.net>
2021-03-16 00:18:43 +0200

    getenv change

--- src/config/config.py
+++ src/config/config.py
@@ -1,8 +1,8 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DB_USERNAME = os.getenv("DB_USERNAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+DB_USERNAME = os.environ.get("DB_USERNAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
